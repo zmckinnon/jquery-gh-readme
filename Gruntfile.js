8aa0a3a8948b0dc1278d7668d7b0ca528fb75f8e
Zachary McKinnon <zach.mckinnon@whiteboard-it.com>
2014-01-23 09:17:55 -0600

    add bump to project

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,17 @@ module.exports = function(grunt) {
           module: true
         }
       }
+    },
+    bump: {
+      options: {
+        files: [ 'package.json', 'bower.json' ],
+        commit: true,
+        commitMessage: 'increment version to %VERSION%',
+        commitFiles: [ 'package.json', 'bower.json' ],
+        createTag: true,
+        push: true,
+        pushTo: 'master'
+      }
     }
   });
 
@@ -43,6 +54,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bump');
 
   // Default tasks
   grunt.registerTask('default', ['concat', 'uglify', 'mocha', 'jshint']);
