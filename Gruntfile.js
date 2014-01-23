074f20e2e44d93776e67b1de9afbf691dd5f627e
Zachary McKinnon <zach.mckinnon@whiteboard-it.com>
2014-01-23 12:31:10 -0600

    add jquery.gh-readme.min.js to bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
         files: [ 'package.json', 'bower.json' ],
         commit: true,
         commitMessage: 'increment version to %VERSION%',
-        commitFiles: [ 'package.json', 'bower.json' ],
+        commitFiles: [ 'package.json', 'bower.json', 'dist/jquery.gh-readme.min.js' ],
         createTag: true,
         push: true,
         pushTo: 'origin'
@@ -49,14 +49,22 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-bump');
 
-  // Default tasks
-  grunt.registerTask('default', ['concat', 'uglify', 'mocha', 'jshint']);
-
+  grunt.registerTask('default', [ 'concat', 'uglify', 'mocha', 'jshint' ]);
+  grunt.registerTask('update-pkg', function () {
+    grunt.config.set('pkg', grunt.file.readJSON('package.json'));
+    grunt.log.ok('Updated pkg configuration.');
+  });
+  grunt.registerTask('build', function (versionType) {
+    versionType = versionType || 'patch';
+    grunt.task.run('bump-only:' + versionType);
+    grunt.task.run('update-pkg');
+    grunt.task.run('uglify');
+    grunt.task.run('bump-commit');
+  });
 };
